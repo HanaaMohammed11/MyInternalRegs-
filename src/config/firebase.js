4b4c57f2b9f4df0e0e1f7fdf8d185631c23d03a1
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-10-16 12:28:11 -0700

    firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,18 +5,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
+//TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAihjm94opoP-p8a-TmLi44AfK2z6CCHSM",
-  authDomain: "mylegislations-and-policies.firebaseapp.com",
-  projectId: "mylegislations-and-policies",
-  storageBucket: "mylegislations-and-policies.appspot.com",
-  messagingSenderId: "633854784630",
-  appId: "1:633854784630:web:8ac5d6f0d7090965209b57",
+  apiKey: "AIzaSyASkW2hoI7IvmnFNU0Mxje-am0OsESaELo",
+  authDomain: "myinternalregs.firebaseapp.com",
+  projectId: "myinternalregs",
+  storageBucket: "myinternalregs.appspot.com",
+  messagingSenderId: "689683351774",
+  appId: "1:689683351774:web:3ed8d57d3c19fba6655a4f",
+  measurementId: "G-GTEL5TJWVW"
 };
 
 // Initialize Firebase
