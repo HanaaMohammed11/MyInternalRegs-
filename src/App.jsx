ed43b83ea26a093f345cc7d82c462a6278e98a46
HanaaMohammed11 <hanaamohammed840@gmail.com>
2024-11-01 18:15:12 -0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -30,37 +30,47 @@ import SubjectsLists from "./pages/Users/Subjects/SubjectList";
 import AdminMatrixInfo from "./pages/Dashboard/Componants/Matrix/MatrixInfo";
 import AdminSubjectInfo from "./pages/Dashboard/Componants/Subjects/AdminSubInfo";
 import IntroPage from "./pages/Login/introPage";
+import Loader from "./pages/Login/loader";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const checkUserInFirestore = async (userId) => {
-      try {
-        const q = query(collection(db, "users"), where("ID", "==", userId)); 
-        const querySnapshot = await getDocs(q);
-
-        if (!querySnapshot.empty) {
-          setIsLoggedIn(true);
-        } else {
-          setIsLoggedIn(false);
-        }
-      } catch (error) {
-        console.error("Error checking Firestore: ", error);
-        setIsLoggedIn(false);
-      }
-    };
-
-    const userId = localStorage.getItem("id");
-    if (userId) {
-      console.log("User ID found: ", userId);
-      checkUserInFirestore(userId);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, [navigate]);
-
+          const [isLoggedIn, setIsLoggedIn] = useState(false);
+          const [loading, setLoading] = useState(true);
+          const navigate = useNavigate();
+        
+          useEffect(() => {
+            const checkUserInFirestore = async (userId) => {
+              try {
+                const q = query(collection(db, "users"), where("ID", "==", userId));
+                const querySnapshot = await getDocs(q);
+        
+                if (!querySnapshot.empty) {
+                  console.log("User found in Firestore:", querySnapshot.docs[0].data());
+                  setIsLoggedIn(true);
+                } else {
+                  console.log("User not found in Firestore with ID:", userId);
+                  setIsLoggedIn(false);
+                }
+              } catch (error) {
+                console.error("Error checking Firestore: ", error);
+                setIsLoggedIn(false);
+              } finally {
+                setLoading(false); 
+              }
+            };
+        
+            const userId = localStorage.getItem("id");
+            if (userId) {
+              console.log("User ID found: ", userId);
+              checkUserInFirestore(userId);
+            } else {
+              setIsLoggedIn(false);
+              setLoading(false); 
+            }
+          }, [navigate]);
+        
+          if (loading) {
+            return <div className="flex justify-center mt-96 items-center" > <Loader/></div>; 
+          }
 
 
   return (
@@ -87,6 +97,8 @@ export default function App() {
           <Route path="/MatrixList" element={<MatrixList />} />
           <Route path="/MatrixEditForm" element={<MatrixEditForm />} />
           <Route path="/MatrixForm" element={<MatrixForm />} />
+          <Route path="*" element={<Home />} /> 
+
         </>
       ) : (
         <>
